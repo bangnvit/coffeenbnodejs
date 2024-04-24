274a9cca416bb2b617e90e84497918268056c416
BangNV <bangnv.it@gmail.com>
2024-04-24 22:53:23 +0700

    ok endpoint: order_new order_cancel order_accept order_refuse order_send

--- test.js
+++ test.js
@@ -3,16 +3,9 @@ const express = require("express");
 const cors = require("cors");
 require('dotenv').config();
 
-// them
-const crypto = require('crypto');
-const axios = require('axios');
-const port = 3006;
-//
-
 const app = express();
 app.use(express.json());
 app.use(cors());
-
 
 const serviceAccount = require(process.env.GOOGLE_APPLICATION_CREDENTIALS);
 
@@ -331,57 +324,7 @@ function getUserTokensFromFirebase() {
   });
 }
 
-// Testing...
-app.post("/api/checkout_momo", async (req, res) => {
-  const accessKey = 'F8BBA842ECF85';
-  const secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
-  const orderInfo = 'Pay with MoMo';
-  const partnerCode = 'MOMO';
-  const redirectUrl = 'http://localhost:3006/success';
-  const ipnUrl = 'http://localhost:3006/success';
-  const requestType = "payWithATM";
-  const amount = '10000';
-  const orderId = partnerCode + new Date().getTime();
-  const requestId = orderId;
-  const extraData = '';
-  const paymentCode = 'T8Qii53fAXyUftPV3m9ysyRhEanUs9KlOPfHgpMR0ON50U10Bh+vZdpJU7VY4z+Z2y77fJHkoDc69scwwzLuW5MzeUKTwPo3ZMaB29imm6YulqnWfTkgzqRaion+EuD7FN9wZ4aXE1+mRt0gHsU193y+yxtRgpmY7SDMU9hCKoQtYyHsfFR5FUAOAKMdw2fzQqpToei3rnaYvZuYaxolprm9+/+WIETnPUDlxCYOiw7vPeaaYQQH0BF0TxyU3zu36ODx980rJvPAgtJzH1gUrlxcSS1HQeQ9ZaVM1eOK/jl8KJm6ijOwErHGbgf/hVymUQG65rHU2MWz9U8QUjvDWA==';
-  const orderGroupId = '';
-  const autoCapture = true;
-  const lang = 'vi';
-  const rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType;
-  const signature = crypto.createHmac('sha256', secretKey)
-      .update(rawSignature)
-      .digest('hex');
-  const requestBody = {
-      partnerCode: partnerCode,
-      partnerName: "Test",
-      storeId: "MomoTestStore",
-      requestId: requestId,
-      amount: amount,
-      orderId: orderId,
-      orderInfo: orderInfo,
-      redirectUrl: redirectUrl,
-      ipnUrl: ipnUrl,
-      lang: lang,
-      requestType: requestType,
-      autoCapture: autoCapture,
-      extraData: extraData,
-      orderGroupId: orderGroupId,
-      signature: signature
-  };
-  const response = await axios.post("https://test-payment.momo.vn/v2/gateway/api/create", requestBody)
-  return res.redirect(response.data.payUrl);
-});
 
-
-app.get("/success", (req, res) => {
-  return res.status(200).json({
-      message: "Success"
-  })
-})
-
-app.listen(port, () => {
-  console.log(`Server listening on port: ${port}`);
-}).on('error', (err) => {
-  console.log(err);
+app.listen(3006, function () {
+  console.log("Server started on port 3006");
 });
